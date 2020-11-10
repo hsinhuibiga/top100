d3411618d353b21ff15e2f486ab456a43e6f8bbc
hsinhuibiga <hsinhuibiga@gmail.com>
2020-11-10 19:48:49 +0800

    Add files via upload

---
+++ Counting Bits.py
@@ -0,0 +1,12 @@
+#Counting Bits
+
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        res = [0] * (num + 1)
+        for i in range(1, num + 1):
+            res[i] = res[i / 2] + i % 2
+        return res
